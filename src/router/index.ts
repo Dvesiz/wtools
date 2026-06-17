ad149d71259f70f8ae78a3116ac22e4e1d1cdb17
Danburen <2964361672@qq.com>
2026-06-17 16:26:36 +0800

    fix: replace router history to hash history.

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/',                name: 'index',           component: () => import('../views/IndexView.vue') },
     { path: '/java-to-ts',      name: 'java-to-ts',      component: () => import('../views/JavaToTsView.vue') },
